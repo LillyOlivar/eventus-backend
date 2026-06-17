be99b53e3022bc3a5bf94862ec4679b3d8608931
Lily Salamanca <lsalamanca@unicauca.com>
2026-06-16 22:58:04 -0500

    fix: remove frontend folder from backend

--- eventus-frontend/src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-// src/environments/environment.prod.ts
-export const environment = {
-  production: true,
-  apiUrl: 'https://tu-backend.railway.app/api/v1', // Cambiar por tu URL de producción
-};
